742c3d1977a8100546448215294ea73e008b5612
shettyrocks <evivekanandashetty@gmail.com>
2025-03-01 22:23:43 +0530

    Adding log4j2

--- WebTestCongress/src/viv/simple/SampleTest.java
+++ WebTestCongress/src/viv/simple/SampleTest.java
@@ -1,12 +1,17 @@
 package viv.simple;
 
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SampleTest {
 	
+	private static final Logger logger = LogManager.getLogger(SampleTest.class);
+
 	@Test
 	public void testfunction()
 	{
+		logger.info("Test method : testfunction");
 		System.out.print("TestNg");
 	}
 
